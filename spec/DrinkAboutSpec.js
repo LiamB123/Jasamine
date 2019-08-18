f248e699e1ebc7c3c799bb2fc1ab9eda1f67e9ab
Ubuntu <ubuntu@ip-172-31-32-27.ec2.internal>
2019-08-18 18:54:06 +0000

    Inintial Commiit

---
+++ spec/DrinkAboutSpec.js
@@ -0,0 +1,7 @@
+
+describe ("Age Test",function(){
+    it("Should Return Can't Tell because age is incorrrect",function(){
+        (Age===0<)
+        expect(return("Sorry. I can't Tekk what drink because that age is incorrrect").toBe(<0))
+    });
+}
